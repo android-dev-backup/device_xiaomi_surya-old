08e95275f87268af24599a911db01ab2cca9e272
SawaMEN <az09209@yandex.ru>
2021-01-21 20:33:30 +0700

    surya: [temp] fix HDR yellow tint

--- product_prop.mk
+++ product_prop.mk
@@ -45,6 +45,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.has_wide_color_display=false \
+    ro.surface_flinger.has_HDR_display=false \
     vendor.display.disable_rotator_downscale=1
 
 # WFD
