dfe524b4b5b7f7a5404e5984f2c6a4c7866d8452
SawaMEN <az09209@yandex.ru>
2021-09-11 00:48:01 +0700

    revert clang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,7 +98,6 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 ifeq ($(TARGET_PREBUILT_KERNEL),)
   TARGET_KERNEL_CONFIG := surya_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
-  TARGET_KERNEL_CLANG_VERSION := latest
   TARGET_KERNEL_SOURCE := kernel/xiaomi/surya
 endif
 
